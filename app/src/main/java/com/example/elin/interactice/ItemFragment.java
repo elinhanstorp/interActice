77d0b1648e55e941056b9195e4542b9e37aa7f0a
Elin Hanstorp <elin.hanstorp@live.com>
2017-04-26 21:54:42 +0200

    Städade upp lite i koden. och la till två knappar på startsidan att kunna koppla en activity till

---
+++ app/src/main/java/com/example/elin/interactice/ItemFragment.java
@@ -0,0 +1,44 @@
+package com.example.elin.interactice;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ItemFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ItemFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ItemFragment extends Fragment {
+    public static ItemFragment newInstance() {
+        ItemFragment fragment = new ItemFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_item, container, false);
+    }
+}
+
--- app/src/main/java/com/example/elin/interactice/MainActivity.java
+++ app/src/main/java/com/example/elin/interactice/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.elin.interactice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.menu_home:
+                               selectedFragment = workoutstart.newInstance();
+                                break;
+                            case R.id.menu_guide:
+                                selectedFragment = GuideFragment.newInstance();
+                                break;
+                            case R.id.menu_settings:
+                            selectedFragment = ItemFragment.newInstance();
+                            break;
+                            case R.id.menu_statistics:
+                                selectedFragment = ItemFragment.newInstance();
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, workoutstart.newInstance());
+        transaction.commit();
+
+        //Used to select an item programmatically
+        //bottomNavigationView.getMenu().getItem(2).setChecked(true);
+
     }
+
 }
